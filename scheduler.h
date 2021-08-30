d9eb6f81f473fe5427906c98b030bb1e132ed857
Vianney Nguyen <nguyenvianney@gmail.com>
2021-08-29 19:54:25 -0600

    Add looper

--- scheduler.h
+++ scheduler.h
@@ -6,19 +6,26 @@
 #include <functional>
 
 struct Task {
-    uint64_t repeatDelay = 0;
-    uint64_t lastRun = 0;
-    std::function<void()> func;
+    uint64_t delay = 0;
+    uint64_t prev = 0;
+    std::function<void(uint64_t)> func;
+
+    Task(uint64_t repeatDelay, uint64_t lastRun, std::function<void(uint64_t)> function);
 };
 
 class Scheduler {
     std::unordered_map<int, Task> tasks;
-    uint64_t prevTime;
 
 public:
-    Scheduler();
-
-    void schedule(int key, uint64_t repeatDelay, std::function<void()> task, bool runImmediately = true);
+    /**
+     * Schedules a task to be run every [repeatDelay] milliseconds.
+     * 
+     * @param key the identifier of this task
+     * @param repeatDelay the delay to run, in milliseconds
+     * @param task the function to run
+     * @param runImmediately true to run immediately, false to run after [repeatDelay] milliseconds
+     */
+    void schedule(int key, uint64_t repeatDelay, std::function<void(uint64_t)> task, bool runImmediately = true);
     void remove(int key);
 
     void loop();
